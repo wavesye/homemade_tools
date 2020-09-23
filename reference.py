5053522948bac8c71349fd666640f3ed90b04b8c
wavesye <55677626+wavesye@users.noreply.github.com>
2020-09-23 17:43:20 +0800

    Delete reference.py

--- reference.py
+++
@@ -1,6 +0,0 @@
-
-with open('/Users/apple/Desktop/reference_list.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not (line.endswith(':') or line.startswith('The')):
-            print(line)
